247f8716a292434ade81fd6c26fdcb1940e06587
ariorh <alextereh09@gmail.com>
2024-02-08 02:36:32 +0400

    v0.0.5 - new main menu font

--- js/MainMenuScene.js
+++ js/MainMenuScene.js
@@ -1,4 +1,7 @@
 // MainMenuScene.js
+
+import { DEFAULT_TEXT_STYLE } from './Styles.js';
+
 export default class MainMenuScene extends Phaser.Scene {
     constructor() {
         super('MainMenuScene');
@@ -7,16 +10,10 @@ export default class MainMenuScene extends Phaser.Scene {
     create() {
         this.cameras.main.setBackgroundColor('#444444');
         // Отображаем название игры или любой другой текст
-        this.add.text(this.cameras.main.centerX, this.cameras.main.centerY - 100, 'Comandor Triangle', {
-            fontSize: '32px',
-            fill: '#fff'
-        }).setOrigin(0.5);
+        this.add.text(this.cameras.main.centerX, this.cameras.main.centerY - 100, 'Comandor Triangle', DEFAULT_TEXT_STYLE).setOrigin(0.5);
 
         // Добавляем кнопку для запуска игры
-        const playButton = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'Play', {
-            fontSize: '32px',
-            fill: '#fff'
-        })
+        const playButton = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'Play', DEFAULT_TEXT_STYLE)
         .setOrigin(0.5)
         .setInteractive({ useHandCursor: true })
         .on('pointerdown', () => this.scene.start('GameScene'));
---
+++ js/Styles.js
@@ -0,0 +1,7 @@
+const DEFAULT_TEXT_STYLE = {
+    fontSize: '32px',
+    fontFamily: '"Press Start 2P"',
+    color: '#ffffff'
+};
+
+export { DEFAULT_TEXT_STYLE };
